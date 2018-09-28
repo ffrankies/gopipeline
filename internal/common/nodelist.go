5f3152e068762222a426a824109ddea846aed6fa
Frank Wanye <wanyef@mail.gvsu.edu>
2018-09-27 20:49:33 -0400

    Added logic for reading in nodelist

--- internal/common/nodelist.go
+++ internal/common/nodelist.go
@@ -1,7 +1,30 @@
+// A collection of functions common to gopipeline, gopipeline/master and gopipeline/worker
 package common
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
-func ReadNodeList(path string) {
-	fmt.Println("Reading the node list")
+// ReadNodeList reads in a file containing the list of nodes on which to run the pipelined module.
+// Each node is expected to be on a new line. No checking is done for whether or not the nodes are valid, however empty
+// lines are filtered out.
+func ReadNodeList(path string) []string {
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	contentsString := string(contents)
+	contentsArray := strings.Split(contentsString, "\n")
+	filteredContentsArray := make([]string, 0)
+	for _, value := range contentsArray {
+		if len(value) > 1 {
+			filteredContentsArray = append(filteredContentsArray, value)
+		}
+	}
+	for _, value := range filteredContentsArray {
+		fmt.Println(value)
+	}
+	return filteredContentsArray
 }
