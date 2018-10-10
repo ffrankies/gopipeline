2c86d054d7ba97f0c3382635a9c3abe2898d0ed1
Frank Wanye <wanyef@mail.gvsu.edu>
2018-10-09 20:05:32 -0400

    Added worker / master separation

--- gopipeline.go
+++ gopipeline.go
@@ -2,6 +2,7 @@
 package gopipeline
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -10,16 +11,31 @@ import (
 	"github.com/ffrankies/gopipeline/types"
 )
 
+// getProcessType obtains the process type from the command line arguments.
+func getProcessType() (processType string, err error) {
+	invalidArgError := errors.New("Must pass in either \"master\" or \"worker\" as the last command-line argument")
+	if len(os.Args) < 2 {
+		err = invalidArgError
+		return
+	}
+	processType = os.Args[len(os.Args)-1]
+	if processType != "master" && processType != "worker" {
+		err = invalidArgError
+		return
+	}
+	return
+}
+
 // Run runs either the master or the worker stage on a single node. Also parses the command-line arguments needed for
 // the worker and/or master
 func Run(functionList []types.AnyFunc) {
-
-	processType := os.Args[1]
-	// flag.StringVar(&processType, "type", "master", "Determines the process type. Can be either of [master, worker]")
-	fmt.Println("Got a command line argument...", processType)
-	return
+	program := os.Args[0]
+	processType, err := getProcessType()
+	if err != nil {
+		panic(err)
+	}
 	if processType == "master" {
-		options := common.NewMasterOptions()
+		options := common.NewMasterOptions(program)
 		master.Run(options, functionList)
 		return
 	}
