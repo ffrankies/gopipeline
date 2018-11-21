c2c1ab3875677c5a95e0c871ba0ef32c03118bd7
Frank Wanye <wanyef@mail.gvsu.edu>
2018-11-21 13:13:58 -0500

    Added the PipelineNodeList type

--- types/node.go
+++ types/node.go
@@ -1,6 +1,43 @@
 package types
 
-// PipelineNode struct refers to a computational PipelineNode. A PipelineNode can be assigned multiple functions, or pipeline stages.
+// PipelineNodeList contains the list of user-provided nodes
+type PipelineNodeList struct {
+	list []*PipelineNode // The list of nodes
+}
+
+// NewPipelineNodeList creates a new empty PipelineNodeList
+func NewPipelineNodeList() *PipelineNodeList {
+	pipelineNodeList := new(PipelineNodeList)
+	return pipelineNodeList
+}
+
+// FindNode finds a particular PipelineNode in the PipelineNodeList. If the Node is not found,
+// findNode creates a new PipelineNode
+func (nodeList *PipelineNodeList) FindNode(nodeAddress string) (pipelineNode *PipelineNode, foundInList bool) {
+	for _, node := range nodeList.list {
+		if node.Address == nodeAddress {
+			pipelineNode = node
+			foundInList = true
+			return
+		}
+	}
+	pipelineNode = NewPipelineNode(nodeAddress, len(nodeList.list))
+	foundInList = false
+	return
+}
+
+// AddNode will add a PipelineNode to the PipelineNodeList
+func (nodeList *PipelineNodeList) AddNode(node *PipelineNode) {
+	nodeList.list = append(nodeList.list, node)
+}
+
+// Length returns the number of nodes in the PipelineNodeList
+func (nodeList *PipelineNodeList) Length() int {
+	return len(nodeList.list)
+}
+
+// PipelineNode struct refers to a computational PipelineNode. A PipelineNode can be assigned multiple functions, or
+// pipeline stages.
 type PipelineNode struct {
 	Address        string           // The internet address of the PipelineNode. Can be DNS, IPv4 or IPv6
 	Position       int              // The position of this PipelineNode in the PipelineNodelist
