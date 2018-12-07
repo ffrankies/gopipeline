a02d545e603f662bff53ead05ae6281ccf2e7bad
Frank Wanye <wanyef@mail.gvsu.edu>
2018-12-06 20:32:42 -0500

    Removed commented-out method

--- worker/firststage.go
+++ worker/firststage.go
@@ -11,25 +11,6 @@ import (
 
 var waitingForStartPipelineMessage = true
 
-// // waitForStartCommand tells a worker to wait for
-// func waitForStartCommand(listener net.Listener) {
-// 	logPrint("Waiting for the start command")
-// 	message := new(types.Message)
-// 	connection, err := listener.Accept()
-// 	defer connection.Close()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	decoder := gob.NewDecoder(connection)
-// 	decoder.Decode(message)
-// 	if message.Description == common.MsgStartWorker {
-// 		logMessage("Starting Pipeline")
-// 	} else {
-// 		logMessage("Received invalid message from: " + message.Sender + " Expected: MsgStartWorker, and instead " +
-// 			" received " + strconv.Itoa(message.Description))
-// 	}
-// }
-
 // runFirstStage runs the function of a worker running the first stage
 func runFirstStage(listener net.Listener, functionList []types.AnyFunc, myID string, registerType interface{}) {
 	go receiveMessages(listener)
