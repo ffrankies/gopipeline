bf6add5ad0bde7777db7368d45e54795b5dcc8c6
Frank Wanye <wanyef@mail.gvsu.edu>
2018-12-08 15:38:03 -0500

    Separated PipelineNode and PipelineNodeList into separate files

---
+++ types/nodelist.go
@@ -0,0 +1,49 @@
+package types
+
+// PipelineNodeList contains the list of user-provided nodes
+type PipelineNodeList struct {
+	list []*PipelineNode // The list of nodes
+}
+
+// NewPipelineNodeList creates a new empty PipelineNodeList
+func NewPipelineNodeList() *PipelineNodeList {
+	pipelineNodeList := new(PipelineNodeList)
+	return pipelineNodeList
+}
+
+// FindNode finds a particular PipelineNode in the PipelineNodeList. If the Node is not found,
+// findNode creates a new PipelineNode
+func (nodeList *PipelineNodeList) FindNode(nodeAddress string) (pipelineNode *PipelineNode, foundInList bool) {
+	for _, node := range nodeList.list {
+		if node.Address == nodeAddress {
+			pipelineNode = node
+			foundInList = true
+			return
+		}
+	}
+	pipelineNode = NewPipelineNode(nodeAddress, len(nodeList.list))
+	foundInList = false
+	return
+}
+
+// AddNode will add a PipelineNode to the PipelineNodeList
+func (nodeList *PipelineNodeList) AddNode(node *PipelineNode) {
+	node.Position = len(nodeList.list) + 1
+	nodeList.list = append(nodeList.list, node)
+}
+
+// Length returns the number of nodes in the PipelineNodeList
+func (nodeList *PipelineNodeList) Length() int {
+	return len(nodeList.list)
+}
+
+// Pop removes and returns the first PipelineNode from the PipelineNodeList list
+func (nodeList *PipelineNodeList) Pop() *PipelineNode {
+	firstNode := nodeList.list[0]
+	if len(nodeList.list) > 1 {
+		nodeList.list = nodeList.list[1:]
+	} else {
+		nodeList.list = make([]*PipelineNode, 0)
+	}
+	return firstNode
+}
