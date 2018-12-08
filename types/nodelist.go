a23ab0c1cdcd8dbae0d3c377601b02bc38a16ee9
Bipasha-banerjee <bips.ban@gmail.com>
2018-12-08 16:53:02 -0500

    List is now public

--- types/nodelist.go
+++ types/nodelist.go
@@ -1,8 +1,8 @@
 package types
 
-// PipelineNodeList contains the list of user-provided nodes
+// PipelineNodeList contains the List of user-provided nodes
 type PipelineNodeList struct {
-	list []*PipelineNode // The list of nodes
+	List []*PipelineNode // The List of nodes
 }
 
 // NewPipelineNodeList creates a new empty PipelineNodeList
@@ -14,21 +14,21 @@ func NewPipelineNodeList() *PipelineNodeList {
 // FindNode finds a particular PipelineNode in the PipelineNodeList. If the Node is not found,
 // findNode creates a new PipelineNode
 func (nodeList *PipelineNodeList) FindNode(nodeAddress string) (pipelineNode *PipelineNode, foundInList bool) {
-	for _, node := range nodeList.list {
+	for _, node := range nodeList.List {
 		if node.Address == nodeAddress {
 			pipelineNode = node
 			foundInList = true
 			return
 		}
 	}
-	pipelineNode = NewPipelineNode(nodeAddress, len(nodeList.list))
+	pipelineNode = NewPipelineNode(nodeAddress, len(nodeList.List))
 	foundInList = false
 	return
 }
 
 // FindNodeWithEnoughMemory finds a node with enough memory to satisfy the requirement of the given stage
 func (nodeList *PipelineNodeList) FindNodeWithEnoughMemory(requirement uint64) *PipelineNode {
-	for _, node := range nodeList.list {
+	for _, node := range nodeList.List {
 		if node.HasEnoughMemory(requirement) {
 			return node
 		}
@@ -38,22 +38,22 @@ func (nodeList *PipelineNodeList) FindNodeWithEnoughMemory(requirement uint64) *
 
 // AddNode will add a PipelineNode to the PipelineNodeList
 func (nodeList *PipelineNodeList) AddNode(node *PipelineNode) {
-	node.Position = len(nodeList.list) + 1
-	nodeList.list = append(nodeList.list, node)
+	node.Position = len(nodeList.List) + 1
+	nodeList.List = append(nodeList.List, node)
 }
 
 // Length returns the number of nodes in the PipelineNodeList
 func (nodeList *PipelineNodeList) Length() int {
-	return len(nodeList.list)
+	return len(nodeList.List)
 }
 
-// Pop removes and returns the first PipelineNode from the PipelineNodeList list
+// Pop removes and returns the first PipelineNode from the PipelineNodeList List
 func (nodeList *PipelineNodeList) Pop() *PipelineNode {
-	firstNode := nodeList.list[0]
-	if len(nodeList.list) > 1 {
-		nodeList.list = nodeList.list[1:]
+	firstNode := nodeList.List[0]
+	if len(nodeList.List) > 1 {
+		nodeList.List = nodeList.List[1:]
 	} else {
-		nodeList.list = make([]*PipelineNode, 0)
+		nodeList.List = make([]*PipelineNode, 0)
 	}
 	return firstNode
 }
