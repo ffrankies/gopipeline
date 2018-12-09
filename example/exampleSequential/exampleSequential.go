178a59c9966691dd89183d8a8b6aeea7d3185779
Frank Wanye <wanyef@mail.gvsu.edu>
2018-12-09 18:22:08 -0500

    Added sequential example

--- example/exampleSequential/exampleSequential.go
+++ example/exampleSequential/exampleSequential.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func hello(num int) {
+	time.Sleep(1 * time.Second)
+	fmt.Println("Hello World from Position: " + strconv.Itoa(num))
+}
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Started execution at", strconv.FormatInt(time.Now().UnixNano(), 10))
+	for i := 0; i < 100; i++ {
+		for i := 0; i < 20; i++ {
+			hello(i)
+		}
+	}
+	fmt.Println("Finished execution at", strconv.FormatInt(time.Now().UnixNano(), 10))
 }
