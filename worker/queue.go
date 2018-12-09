2f084f77b6cde1b79d71fae74f022c979e761600
Bipasha-banerjee <bips.ban@gmail.com>
2018-12-08 20:28:04 -0500

    Added a function to wait until queue is empty

--- worker/queue.go
+++ worker/queue.go
@@ -17,18 +17,20 @@ func (n *Element) MessageString() string {
 
 // Queue Struct
 type Queue struct {
-	elements  []interface{}
-	length    int
-	semaphore chan int
-	mutex     *sync.Mutex
+	elements     []interface{}
+	length       int
+	semaphore    chan int
+	mutex        *sync.Mutex
+	emptyChannel chan int
 }
 
 // makeQueue makes a new queue and returns it along with its size.
 func makeQueue() *Queue {
 	return &Queue{
-		elements:  make([]interface{}, 0),
-		semaphore: make(chan int, 9999), // Make a buffered channel with a high buffer
-		mutex:     &sync.Mutex{},
+		elements:     make([]interface{}, 0),
+		semaphore:    make(chan int, 9999), // Make a buffered channel with a high buffer
+		mutex:        &sync.Mutex{},
+		emptyChannel: nil,
 	}
 }
 
@@ -46,6 +48,11 @@ func (q *Queue) Pop() interface{} {
 	q.mutex.Lock()
 	result := q.elements[0]
 	q.elements = q.elements[1:]
+	if q.emptyChannel != nil {
+		if len(q.elements) == 0 {
+			q.emptyChannel <- 1
+		}
+	}
 	q.mutex.Unlock()
 	return result
 }
@@ -56,4 +63,11 @@ func (q *Queue) GetLength() int {
 	length := len(q.elements)
 	q.mutex.Unlock()
 	return length
+}
+
+// WaitUntilEmpty uses a blocking channel to wait until the queue is empty
+func (q *Queue) WaitUntilEmpty() {
+	q.emptyChannel = make(chan int, 1)
+	<-q.emptyChannel
+	q.emptyChannel = nil
 }
